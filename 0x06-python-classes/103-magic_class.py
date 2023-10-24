ba27c401cfa647496f7ff3116bbbfa33c078720a
KIHALAya <kihalaya00@gmail.com>
2023-10-24 14:37:00 -0700

    Task 10: Python class MagicClass

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,24 @@
+import math
+
+
+class MagicClass:
+    """A class that replicates the provided bytecode functionality."""
+
+    def __init__(self, radius=0):
+        """Initialize the MagicClass.
+
+        Args:
+            radius: A number (int or float)
+            representing the radius of the circle.
+        """
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        """Calculate and return the area of the circle."""
+        return self.__radius ** 2 * math.pi
+
+    def circumference(self):
+        """Calculate and return the circumference of the circle."""
+        return 2 * math.pi * self.__radius
