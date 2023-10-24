c443f0636e64637227a1f726135c7a29c88b0435
KIHALAya <kihalaya00@gmail.com>
2023-10-24 12:51:04 -0700

    Task 1:class Square that defines a square by Private instance attribute: size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""The Square class."""
+
+class Square:
+    """Represent Square object."""
+
+    def __init__(self, size):
+        """ Define a new Square Object"""
+        self.__size = size
