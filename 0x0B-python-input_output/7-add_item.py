cc68a7fa835496d7f8551d1f86cebad6fc718b41
KIHALAya <kihalaya00@gmail.com>
2023-11-07 12:30:38 -0800

    Task 7:  script that adds all arguments to a Python list, and then save them to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
+"""Script to append command line arguments to a
+Python list and save them to a file."""
+
 import sys
-from os import path
 
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
 
-filename = 'add_item.json'
+if __name__ == "__main__":
+    save_to_json = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json = __import__('6-load_from_json_file').load_from_json_file
 
-if not path.exists(filename):
-    data = []
-else:
-    data = load_from_json_file(filename)
-
-data.extend(sys.argv[1:])
-save_to_json_file(data, filename)
+    try:
+        stored_items = load_from_json("add_item.json")
+    except FileNotFoundError:
+        stored_items = []
+    stored_items.extend(sys.argv[1:])
+    save_to_json(stored_items, "add_item.json")
