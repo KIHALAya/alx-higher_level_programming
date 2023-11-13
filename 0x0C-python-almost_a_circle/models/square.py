d1c41cb187aad4e6eb6dd05b7dcd9e1c34e7af81
KIHALAya <kihalaya00@gmail.com>
2023-11-13 12:37:04 -0800

    Task 10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Square Module """
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ Square class that inherits from Rectangle """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """ Initializes a Square instance
+
+        Args:
+            size (int): size of the square
+            x (int): x-coordinate of the square
+            y (int): y-coordinate of the square
+            id (int): id of the square
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ Returns the string representation of the Square instance """
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width
+        )
