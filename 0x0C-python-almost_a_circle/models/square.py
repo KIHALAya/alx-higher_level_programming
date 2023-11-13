88e94ecee5ffdba626f702df4d71e0e7b0cfdc30
KIHALAya <kihalaya00@gmail.com>
2023-11-13 12:41:48 -0800

    Task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -17,6 +17,17 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """ Getter for size """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ Setter for size """
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """ Returns the string representation of the Square instance """
         return "[Square] ({}) {}/{} - {}".format(
