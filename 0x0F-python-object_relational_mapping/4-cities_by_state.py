e569873e32b41ed75e05bd8adce1a6f545c30214
KIHALAya <kihalaya00@.com>
2024-01-14 03:33:02 -0800

    Task 4 -  script that lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
         password=arguments[1],
         db=arguments[2]
     )
-    query = """SELECT cities.id, states.name, cities.name
+    query = """SELECT cities.id, cities.name, states.name
                FROM cities  JOIN states
                ON cities.state_id = states.id
                ORDER BY cities.id"""
