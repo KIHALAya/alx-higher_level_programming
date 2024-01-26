62313e472fa332d1cee44308ad1c3d494c4cf5a8
KIHALAya <kihalaya00@gmail.com>
2024-01-26 11:26:53 -0800

    Task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,15 +4,11 @@
 
 
 if __name__ == "__main__":
-    import urllib.request
+    from urllib.request import urlopen
 
-    with urllib.request.urlopen(
-            "https://alx-intranet.hbtn.io/status") as response:
+    with urlopen("https://alx-intranet.hbtn.io/status") as response:
         content = response.read()
-        print(
-            """Body response:
-            - type: {}
-            - content: {}
-            - utf8 content: {}""".format(
-                type(content), content, content.decode('utf-8'))
-        )
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
