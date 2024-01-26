8ee29fbcdf918829eef1003d2b7b9bd8df392283
KIHALAya <kihalaya00@gmail.com>
2024-01-26 09:03:30 -0800

    Task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,6 +5,7 @@
 
 if __name__ == "__main__":
     from urllib.request import urlopen
+
     with urlopen("https://alx-intranet.hbtn.io/status") as response:
         content = response.read()
         print(
