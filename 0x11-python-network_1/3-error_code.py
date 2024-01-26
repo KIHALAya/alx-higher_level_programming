46bb94338beaab8db84c689337713d44b82f8f14
KIHALAya <kihalaya00@gmail.com>
2024-01-26 12:57:47 -0800

    Task 5

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" 3-error_code.py - Python script that takes in a URL,
+sends a request to the URL and displays the body of the response
+(decoded in utf-8)."""
+
+
+if __name__ == "__main__":
+    from urllib.request import urlopen
+    from urllib.error import HTTPError
+    import sys
+
+    args = sys.argv[1:]
+    try:
+        with urlopen(args[0]) as response:
+            content = response.read().decode('utf-8')
+            print(content)
+    except HTTPError as e:
+        print(f"Error code: {e.code}")
