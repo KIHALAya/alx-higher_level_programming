2ad908686d8bb8bb5b9cbbe01b043c9bf2f057d3
KIHALAya <kihalaya00@gmail.com>
2024-01-26 08:31:17 -0800

    Task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-
 """ 0-hbtn_status.py - Python script that
-fetches https://alx-intranet.hbtn.io/status"""
+ fetches https://alx-intranet.hbtn.io/status"""
 
-from urllib.request import urlopen
-url = "https://alx-intranet.hbtn.io/status"
-response = urlopen(url)
-content = response.read()
-print(
-    """Body response:
-    - type: {}
-    - content: {}
-    - utf8 content: {}""".format(
-        type(content), content, content.decode('utf-8'))
-)
+
+if __name__ == "__main__":
+    from urllib.request import urlopen
+    url = "https://alx-intranet.hbtn.io/status"
+    response = urlopen(url)
+    content = response.read()
+    print(
+        """Body response:
+        - type: {}
+        - content: {}
+        - utf8 content: {}""".format(
+            type(content), content, content.decode('utf-8'))
+    )
