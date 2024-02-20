c787fae83d137182a3f6b97879ab5a84cfbbf005
KIHALAya <kihalaya00@gmail.com>
2024-02-20 11:26:31 -0800

    script that gets the contents of a webpage and stores it in a file.

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const request = require('request');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  }
+  if (response.statusCode !== 200) {
+    console.error(`Error: ${response.statusCode}`);
+  }
+  fs.writeFile(filePath, body, 'utf-8', (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+});
